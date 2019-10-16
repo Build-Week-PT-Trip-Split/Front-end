088a74a675caa2d549c3a526847481ce26acfbc9
Patrick Shushereba <pshushereba@gmail.com>
2019-10-16 12:23:44 -0700

    Added Redux for state management.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducer);
 
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>, document.getElementById('root'));
 
