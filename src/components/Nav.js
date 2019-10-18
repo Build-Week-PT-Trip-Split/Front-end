7eb08f9baba18313889837d1d7f71e11667c219f
Jake <panamajake86@gmail.com>
2019-10-17 19:43:26 -0600

    added the Routes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import Logo from '../assets/logo.png';
 
-export default function NavBar () {
+export default function NavBar() {
     return (
-        <div className='navBar'>
-            <Link to='/profile'>My Profile</Link>
-            <Link to='/friends'>My Friends</Link>
-            <Link to='/trips'>My Trips</Link>
-            <Link to='/login'>Logout</Link>
-            <img src={Logo} alt='Trip Split Logo' />
+        <div>
+            <div>
+                <Route exact path='/profile' />
+                <Route exact path='/friends' />
+                <Route exact path='/trips' />
+                <Route exact path='/login' />
+            </div>
+            <div className='navBar'>
+                <Link to='/profile'>My Profile</Link>
+                <Link to='/friends'>My Friends</Link>
+                <Link to='/trips'>My Trips</Link>
+                <Link to='/login'>Logout</Link>
+                <img src={Logo} alt='Trip Split Logo' />
+            </div>
         </div>
     );
 };
