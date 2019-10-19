04b10449aa3acdd9906602aeb01a8932652b4fa6
Jake <panamajake86@gmail.com>
2019-10-18 21:03:08 -0600

    Finally finished those components. I may have gone a little overboard with them but I'm sure that the others will get them to work.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
-import ExpenseList from './components/expenses/ExpenseList';
-// import Expense from './components/expenses/Expense';
-// import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Trip Split</h1>
-      <ExpenseList />
-      {/* <Route path='/expense/:id' component={Expense} /> */}
     </div>
   );
 }
