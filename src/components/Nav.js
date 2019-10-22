6237600fda95dd8cd745985936e0c26f0189ffb7
sofialevin <slevin@M-4876.local>
2019-10-21 22:49:09 -0700

    Added some navbar styling.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import Logo from '../assets/logo.png';
 
+import {Navbar, NavbarBrand} from 'reactstrap';
+
 export default function NavBar(props) {
 
     const handleLogout = (event) => {
@@ -12,17 +14,17 @@ export default function NavBar(props) {
 
     return (
         <div>
-            <div className='navBar'>
-                <img src={Logo} alt='Trip Split Logo' />
+            <Navbar>
+                <NavbarBrand><img src={Logo} alt='Trip Split Logo' /><span>Trip Split</span></NavbarBrand>
                 {/* Shows nav with logout button only when you're logged in */}
                 {props.isAuthenticated ? 
-                <React.Fragment>
+                <div className="navbar-links">
                     <Link to='/trips'>My Trips</Link>
                     <Link to='/friends'>My Friends</Link>
                     <Link to='/profile'>My Profile</Link>
                     <a href="" onClick={handleLogout}>Logout</a>
-                </React.Fragment> : <React.Fragment></React.Fragment> }
-            </div>
+                </div> : <React.Fragment></React.Fragment> }
+            </Navbar>
             <Route exact path='/profile' />
             <Route exact path='/friends' />
             <Route exact path='/trips' />
