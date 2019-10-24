5a59f03af546d7c8cb9ce35da49053bf3707cc6f
Patrick Shushereba <pshushereba@gmail.com>
2019-10-24 07:36:49 -0700

    Connected TripDetail to Redux Store.

--- src/components/trips/TripDetail.js
+++ src/components/trips/TripDetail.js
@@ -1,32 +1,41 @@
 import React from 'react'
+import {connect} from 'react-redux';
 import ExpenseList from '../expenses/ExpenseList';
 
 const TripDetail = (props) => {
+    console.log("In TripDetail", props)
     return (
         <div>
-            <h1>{props.trip.name}</h1>
+            <h1>{props.trips.name}</h1>
             {(() => {
-                if (props.trip.complete == 1) {
+                if (props.trips.complete == 1) {
                     return (<h3>Status: <span className='completed'>Completed</span></h3>)
                 } else {
                     return (<h3>Status: <span className='open'>Open</span></h3>)
                 }
             })()}
-            <img src={props.trip.img} alt="" />
-            <p>Date of Trip: {props.trip.date}</p>
-            <ExpenseList trip={props.trip} />
-            <p>Cost: {props.trip.base_cost}</p>
-            {props.trip.participants.map(part => {
+            <img src={props.trips.img} alt="" />
+            <p>Date of Trip: {props.trips.date}</p>
+            <ExpenseList trip={props.trips} />
+            <p>Cost: {props.trips.base_cost}</p>
+            {/* {props.trips.participants.map(part => {
                 return (
                     <div>
                         <p>Name: {part.user.name}</p>
                         <img src={part.user.src} />
                     </div>
                 )
-            })}
+            })} */}
             <button>Edit Trip</button>
         </div>
     )
 }
 
-export default TripDetail;
+const mapStateToProps = (state) => {
+    console.log(state)
+    return {
+        trips: state.tripReducer.trips
+    }
+}
+
+export default connect(mapStateToProps, {})(TripDetail);
--- src/utils/axiosAuth.js
+++ src/utils/axiosAuth.js
@@ -4,7 +4,7 @@ const axiosWithAuth = () => {
     const token = localStorage.getItem("token");
 
     return axios.create({
-        baseURL: 'https://tripsplitr.herokuapp.com',
+        baseURL: 'https://tripsplit-1022.herokuapp.com/',
         headers: {
             Authorization: token
         }
