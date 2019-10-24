c3484968dde6c087132614d8828786db64e60357
mohdraz <noorinfotechinc@gmail.com>
2019-10-23 22:04:31 -0500

    expense detail component

---
+++ src/components/expenses/ExpenseDetail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ExpenseDetail = ({expDetail}) => {
+
+    const { expense_name, total_expense_price, primary_paid, id, created_at, tripName, tripParticipants } = expDetail;
+
+    return (
+        <div className='expenseDetail'>
+            <Link to={`/expense/${id}`}>
+                <h3>{expense_name}</h3>
+            </Link>
+            <p>Amount: {total_expense_price}</p>
+            <p>Paid By: {primary_paid}</p>
+            <p>Expense Created on: {created_at} </p>
+            <p>Trip Name: {tripName} </p>
+            <p>Number of Peopl Participated: {tripParticipants} </p>
+            <p>Cost Per Participant: {total_expense_price/tripParticipants} </p>
+            detail
+        </div>
+    );
+};
+
+export default ExpenseDetail;
